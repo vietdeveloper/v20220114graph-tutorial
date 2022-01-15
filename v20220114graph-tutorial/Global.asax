3d075d7397f8d626638f3efabdd2cf011611dddd
Viet <vietdeveloper@live.com>
2022-01-14 16:52:40 -0800

    Add project files.

---
+++ v20220114graph-tutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="v20220114graph_tutorial.MvcApplication" Language="C#" %>
---
+++ v20220114graph-tutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(v20220114graph_tutorial.Startup))]
+namespace v20220114graph_tutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
